de997be002dc4605ab5a3ef63fa5f237556ed291
Cristea Alexandru <alex.p.cristea@gmail.com>
2022-12-06 20:45:50 +0200

    Update AvailableMeals.js
    Update the AvailableMeals component so I bring the menu data from a database

--- src/components/meals/AvailableMeals.js
+++ src/components/meals/AvailableMeals.js
@@ -1,38 +1,34 @@
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 import Card from "../UI/Card";
-
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+import { useEffect, useState } from "react";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem key={meal.id} meal={meal} />
-  ));
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://food-order-app-2e0a3-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+      );
+      const responesData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responesData) {
+        loadedMeals.push({
+          id: key,
+          name: responesData[key].name,
+          description: responesData[key].description,
+          price: responesData[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+    };
+    fetchMeals();
+  }, []);
+
+  const mealsList = meals.map((meal) => <MealItem key={meal.id} meal={meal} />);
   return (
     <section className={classes.meals}>
       <Card>
